ce90bb3e679c1a99f7277fa4cdf32ad936dfa093
Timothy DeHerrera <tim.deh@pm.me>
2021-06-06 21:05:26 -0600

    add overlay

--- flake.nix
+++ flake.nix
@@ -4,18 +4,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in {
-      packages.nix-eval-lsp = pkgs.callPackage
-        ({ stdenv, lib, rustPlatform, fetchFromGitHub }:
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      pkg = { stdenv, lib, rustPlatform, fetchFromGitHub }:
         rustPlatform.buildRustPackage {
           pname = "nix-eval-lsp";
           version = "0.1.0";
           src = ./.;
           cargoSha256 = "sha256-OoHGx9RLWahJ11z9EmnnJDj/b2GJhQgJslTmSVuxc7Y=";
           RUSTC_BOOTSTRAP = 1;
-        }) { };
-      defaultPackage = self.packages.${system}.nix-eval-lsp;
-    }
-  );
+        };
+    in flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.nix-eval-lsp = pkgs.callPackage pkg { };
+        defaultPackage = self.packages.${system}.nix-eval-lsp;
+      }) // {
+        overlay = final: prev: { nix-eval-lsp = prev.callPackage pkg { }; };
+      };
 }
